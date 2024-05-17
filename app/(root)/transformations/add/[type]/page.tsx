bd8bbac4d02643dc7af5c72318c4a62a8d816781
Nguyễn Hưng <nguyenhung@MacBook-Air-cua-Nguyen.local>
2024-05-17 23:02:01 +0700

    add file modifile

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,4 @@
 import Header from "@/components/shared/Header";
-import TransformationForm from "@/components/shared/TransformationForm";
 import { transformationTypes } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
@@ -23,15 +22,7 @@ const AddTransformationTypePage = async ({
   }
 
   return (
-    <>
-      <Header title={transformation.title} subtitle={transformation.subTitle} />
-      <TransformationForm
-        action="Add"
-        userId={user._id}
-        type={transformation.type as TransformationTypeKey}
-        creditBalance={user.creditBalance}
-      />
-    </>
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
   );
 };
 

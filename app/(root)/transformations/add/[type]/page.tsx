c5694970a3680dc9aafccc1c0c331807bcc419b5
Nguyễn Hưng <nguyenhung@MacBook-Air-cua-Nguyen.local>
2024-05-17 22:47:54 +0700

    add file modfile

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
-const AddTransformationTypePage = () => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!userId) {
+    redirect("/sign-in");
+    return null; // Ensure that the function exits after redirecting
+  }
+
+  const user = await getUserById(userId);
+  if (!user) {
+    redirect("/sign-in");
+    return null; // Ensure that the function exits after redirecting
+  }
+
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action="Add"
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
+};
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
